b3a2687d1d8f5ebe45c64a66a29e5201e0c6f03b
Sanjeev Rai <srai089@gmail.com>
2024-03-13 13:13:19 +0530

    update subjects

--- src/helper/classOptions.js
+++ src/helper/classOptions.js
@@ -1,6 +1,6 @@
 
 export const classOptions= ["LKG", "UKG", "Class-1", "Class-2", "Class-3", "Class-4", "Class-5", "Class-6", "Class-7", "Class-8", "Class-9", "Class-10", "Class-11", "Class-12"];
 
-export const allSubList= ['Hindi', 'English', "Biology", "Social Science","Science","Physics", "Drawing", "Moral Education", "Physical Education", 'Sanskrit', 'Computer', 'Maths', 'Chemistry'];
+export const allSubList= ['Hindi', 'English', "Biology", "Social Science","Science","Physics", "Drawing", "Moral Education", "Physical Education", 'Sanskrit', 'Computer', 'Maths', 'Chemistry', 'Accountancy', 'Business Studies', 'Economics'];
 
 export const allExams= ['pa1', 'pa2', 'pa3', 'halfYearly', 'annual'];
